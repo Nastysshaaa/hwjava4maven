2037c17d2d845c30f5afe4765c457215716c1afe
Anastasiia <anastasia994@yandex.ru>
2023-03-03 20:59:14 +0500

    "Success attempt"

--- src/test/java/CountServiceParameterizedTest.java
+++ src/test/java/CountServiceParameterizedTest.java
@@ -1,11 +1,16 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import ru.netology.javaqa.CountService;
 
 public class CountServiceParameterizedTest {
     @ParameterizedTest
-    @CsvFileSource(files = "csv/test/resources/count.csv")
+    //@CsvSource({
+    //        "3, 10_000, 3_000, 20_000",
+    //        "2, 100_000, 60_000, 150_000"
+    //})
+    @CsvFileSource(files = "src/test/resources/count.csv")
     public void testCalculateCountMonths(int expected, int income, int expenses, int threshold) {
         CountService service = new CountService();
         int countmonths = service.calculate(income, expenses, threshold);
